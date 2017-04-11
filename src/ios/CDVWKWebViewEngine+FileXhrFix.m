593eb98a155bd1f970276222be8c6bfa757568f4
Matt Ray <themattray@gmail.com>
2017-04-11 10:49:49 -0400

    Bug fix

--- src/ios/CDVWKWebViewEngine+FileXhrFix.m
+++ src/ios/CDVWKWebViewEngine+FileXhrFix.m
@@ -14,11 +14,13 @@
     SEL selector = NSSelectorFromString(@"createConfigurationFromSettings:");
     Method originalMethod = class_getInstanceMethod([CDVWKWebViewEngine class], selector);
     IMP originalImp = method_getImplementation(originalMethod);
+    typedef WKWebViewConfiguration* (*send_type)(id, SEL , NSDictionary*);
+    send_type originalImpSend = (send_type)originalImp;
     
     IMP newImp = imp_implementationWithBlock(^(id _self, NSDictionary* settings){
         // Get the original configuration
-        WKWebViewConfiguration* configuration = originalImp(_self, selector, settings);
-        
+        WKWebViewConfiguration* configuration = originalImpSend(_self, selector, settings);
+
         // allow access to file api
         @try {
             [configuration.preferences setValue:@TRUE forKey:@"allowFileAccessFromFileURLs"];
